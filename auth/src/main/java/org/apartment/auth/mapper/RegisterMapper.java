1b7e32b2d03875fba331bd6b0957a8b29b08a996
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-03 23:33:11 +0300

    feat: add mapper

---
+++ auth/src/main/java/org/apartment/auth/mapper/RegisterMapper.java
@@ -0,0 +1,16 @@
+package org.apartment.auth.mapper;
+
+import org.apartment.auth.dto.RegisterRequestDto;
+import org.apartment.auth.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface RegisterMapper {
+    RegisterMapper INSTANCE = Mappers.getMapper(RegisterMapper.class);
+
+    User toEntity(RegisterRequestDto registerRequest);
+
+    RegisterRequestDto toDto(User user);
+}
