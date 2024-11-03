eb4574779c1e8b4968150e6da4de93629b978dbf
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-03 23:44:47 +0300

    feat: add auth and user controller

---
+++ auth/src/main/java/org/apartment/auth/controller/AuthController.java
@@ -0,0 +1,53 @@
+package org.apartment.auth.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.apartment.auth.dto.AuthenticationRequestDto;
+import org.apartment.auth.dto.AuthenticationResponseDto;
+import org.apartment.auth.dto.RegisterRequestDto;
+import org.apartment.auth.entity.User;
+import org.apartment.auth.mapper.RegisterMapper;
+import org.apartment.auth.service.AuthService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@Slf4j
+public class AuthController {
+
+    private final AuthService service;
+
+    public AuthController(AuthService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponseDto> addNewUser(
+            @RequestBody @Valid RegisterRequestDto registerRequest
+    ) {
+        User user = RegisterMapper.INSTANCE.toEntity(registerRequest);
+
+        return ResponseEntity.ok(service.register(user));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponseDto> authenticate(
+            @RequestBody @Valid AuthenticationRequestDto request
+    ) {
+        return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public ResponseEntity<AuthenticationResponseDto> refreshToken(
+            HttpServletRequest request
+    ) {
+        AuthenticationResponseDto authResponse = service.refreshToken(request);
+
+        return ResponseEntity.ok(authResponse);
+    }
+}
---
+++ auth/src/main/java/org/apartment/auth/controller/UserController.java
@@ -0,0 +1,31 @@
+package org.apartment.auth.controller;
+
+import org.apartment.auth.dto.ChangePasswordRequestDto;
+import org.apartment.auth.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> changePassword(
+            @RequestBody ChangePasswordRequestDto request,
+            Principal connectedUser
+    ) {
+        service.changePassword(request, connectedUser);
+        return ResponseEntity.ok().build();
+    }
+}
