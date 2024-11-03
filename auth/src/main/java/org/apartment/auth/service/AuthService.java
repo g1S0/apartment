7f0573105de86f827342ff6620080baa5afd3dbb
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-03 23:41:24 +0300

    feat: add auth service

---
+++ auth/src/main/java/org/apartment/auth/service/AuthService.java
@@ -0,0 +1,125 @@
+package org.apartment.auth.service;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.apartment.auth.dto.AuthenticationRequestDto;
+import org.apartment.auth.dto.AuthenticationResponseDto;
+import org.apartment.auth.entity.Token;
+import org.apartment.auth.entity.TokenType;
+import org.apartment.auth.entity.User;
+import org.apartment.auth.repository.TokenRepository;
+import org.apartment.auth.repository.UserRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@AllArgsConstructor
+@Slf4j
+public class AuthService {
+    private final UserRepository repository;
+    private final TokenRepository tokenRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponseDto register(User user) {
+        log.debug("Registering user with email: {}", user.getEmail());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        var savedUser = repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        saveUserToken(savedUser, jwtToken);
+
+        log.info("User registered successfully: {}", user.getEmail());
+        return AuthenticationResponseDto.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    public AuthenticationResponseDto authenticate(AuthenticationRequestDto request) {
+        log.debug("Authenticating user with email: {}", request.getEmail());
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        var jwtToken = jwtService.generateToken(user);
+        var refreshToken = jwtService.generateRefreshToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
+
+        log.info("User authenticated successfully: {}", request.getEmail());
+        return AuthenticationResponseDto.builder()
+                .accessToken(jwtToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        log.debug("Saving token for user: {}", user.getEmail());
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        log.debug("Revoking all tokens for user: {}", user.getEmail());
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+    public AuthenticationResponseDto refreshToken(HttpServletRequest request) {
+        log.debug("Refreshing token");
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            log.error("Missing or invalid Authorization header");
+            throw new RuntimeException("Missing or invalid Authorization header");
+        }
+
+        String refreshToken = authHeader.substring(7);
+        String userEmail = jwtService.extractUsername(refreshToken);
+
+        if (userEmail != null) {
+            var user = this.repository.findByEmail(userEmail)
+                    .orElseThrow(() -> new RuntimeException("User not found"));
+
+            if (jwtService.isTokenValid(refreshToken, user)) {
+                String accessToken = jwtService.generateToken(user);
+                revokeAllUserTokens(user);
+                saveUserToken(user, accessToken);
+
+                log.info("Token refreshed successfully for user: {}", userEmail);
+                return AuthenticationResponseDto.builder()
+                        .accessToken(accessToken)
+                        .refreshToken(refreshToken)
+                        .build();
+            } else {
+                log.error("Invalid refresh token for user: {}", userEmail);
+                throw new RuntimeException("Invalid refresh token");
+            }
+        } else {
+            log.error("Invalid refresh token");
+            throw new RuntimeException("Invalid refresh token");
+        }
+    }
+}
