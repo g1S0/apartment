23ea02891d0ffaf548d45c6e2a8c88fea59f4c8f
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-03 23:33:00 +0300

    feat: add DTO for endpoints

---
+++ auth/src/main/java/org/apartment/auth/dto/ChangePasswordRequestDto.java
@@ -0,0 +1,11 @@
+package org.apartment.auth.dto;
+
+import lombok.Data;
+
+@Data
+
+public class ChangePasswordRequestDto {
+    private String currentPassword;
+    private String newPassword;
+    private String confirmationPassword;
+}
---
+++ auth/src/main/java/org/apartment/auth/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package org.apartment.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto<T> {
+    private T data;
+}
