cb47427f698e7675034a96320941edf5055c0f60
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-13 16:55:08 +0300

    refactor: rename method

--- auth/src/main/java/org/apartment/auth/repository/TokenRepository.java
+++ auth/src/main/java/org/apartment/auth/repository/TokenRepository.java
@@ -10,11 +10,12 @@ import org.springframework.data.repository.query.Param;
 
 public interface TokenRepository extends JpaRepository<Token, Integer> {
     @Query(value = """
-      select t from Token t inner join User u\s
-      on t.user.id = u.id\s
-      where u.id = :id and (t.expired = false or t.revoked = false)\s
-      """)
+            select t from Token t inner join User u\s
+            on t.user.id = u.id\s
+            where u.id = :id and (t.revoked = false)\s
+            """)
     List<Token> findAllValidTokenByUser(@Param("id") Integer id);
 
-    Optional<Token> findByToken(String token);
+    @Query("select t from Token t where t.token = :token")
+    Optional<Token> findTokenByValue(String token);
 }
