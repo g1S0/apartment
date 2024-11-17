54c4cbb7734863490a34d2102f6775ba5e05b57f
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-17 14:58:52 +0300

    refactor: refactor EmailNotificationService

--- email-notification/src/main/java/org/apartment/service/EmailNotificationService.java
+++ email-notification/src/main/java/org/apartment/service/EmailNotificationService.java
@@ -2,45 +2,58 @@ package org.apartment.service;
 
 import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
+import org.apartment.dto.EmailDetailsDto;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @Slf4j
 public class EmailNotificationService {
 
-  private final JavaMailSender mailSender;
-
   @Value("${spring.mail.username}")
   private String username;
 
-  public EmailNotificationService(JavaMailSender mailSender) {
+  private final JavaMailSender mailSender;
+  private final SpringTemplateEngine templateEngine;
+
+  public EmailNotificationService(JavaMailSender mailSender, SpringTemplateEngine templateEngine) {
     this.mailSender = mailSender;
+    this.templateEngine = templateEngine;
   }
 
   @KafkaListener(topics = "email_topic", groupId = "email_group")
-  public void sendRegistrationSuccessEmail(String toEmail) {
+  public void handleEmailNotification(String toEmail) {
+    String subject = "TApartment";
+    String messageBody = buildRegistrationSuccessEmailBody();
+
+    EmailDetailsDto emailDetails = new EmailDetailsDto(toEmail, subject, messageBody);
+    sendEmail(emailDetails);
+  }
+
+  private void sendEmail(EmailDetailsDto emailDetails) {
     MimeMessage message = mailSender.createMimeMessage();
 
     try {
       MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
       helper.setFrom(username);
-      helper.setTo(toEmail);
-      helper.setSubject("TApartment");
-
-      String htmlMsg = "<div style='text-align: center;'>" + "<h2>Congratulations!</h2>"
-          +
-          "<p>Your registration was successful.</p>" + "</div>";
-
-      helper.setText(htmlMsg, true);
+      helper.setTo(emailDetails.getToEmail());
+      helper.setSubject(emailDetails.getSubject());
+      helper.setText(emailDetails.getMessageBody(), true);
 
       mailSender.send(message);
-      log.info("Registration success email sent to: {}", toEmail);
+      log.info("Email sent to: {}", emailDetails.getToEmail());
     } catch (Exception e) {
-      log.error("Error sending email to {}: {}", toEmail, e.getMessage());
+      log.error("Error sending email to {}: {}", emailDetails.getToEmail(), e.getMessage());
     }
+  }
+
+  private String buildRegistrationSuccessEmailBody() {
+    Context context = new Context();
+    return templateEngine.process("registrationSuccessEmail", context);
   }
 }
