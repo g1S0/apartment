005fe46a79dfe7384a76a7205b714c972ba721c6
ddasfasdfs <glibsoldatenko@gmail.com>
2024-11-17 17:33:56 +0300

    feat: check if search value is null

--- real-estate/src/main/java/org/apartment/service/PropertySearchService.java
+++ real-estate/src/main/java/org/apartment/service/PropertySearchService.java
@@ -22,15 +22,24 @@ public class PropertySearchService {
 
   public List<Property> searchProperties(String keyword, BigDecimal minPrice, BigDecimal maxPrice,
                                          LocalDateTime startDate, LocalDateTime endDate) {
-    log.info(
-        "Starting search for properties with keyword: {}, price range: {} - {}, date range: {} - {}",
-        keyword, minPrice, maxPrice, startDate, endDate);
+
+    final String effectiveKeyword = (keyword != null) ? keyword : "";
+    final BigDecimal effectiveMinPrice = (minPrice != null) ? minPrice : BigDecimal.ZERO;
+    final BigDecimal effectiveMaxPrice =
+        (maxPrice != null) ? maxPrice : new BigDecimal("1000000000.0");
+    final LocalDateTime effectiveStartDate = (startDate != null) ? startDate : LocalDateTime.MIN;
+    final LocalDateTime effectiveEndDate = (endDate != null) ? endDate : LocalDateTime.MAX;
+
+    log.info("Starting search for properties with keyword: {}, price range: {} - {}, "
+            + "date range: {} - {}", effectiveKeyword, effectiveMinPrice, effectiveMaxPrice,
+        effectiveStartDate, effectiveEndDate);
 
     SearchSession searchSession = Search.session(entityManager);
 
-    return searchSession.search(Property.class).where(
-        f -> f.bool().must(f.match().fields("title", "description", "city").matching(keyword))
-            .must(f.range().field("price").between(minPrice, maxPrice))
-            .must(f.range().field("createdAt").between(startDate, endDate))).fetchHits(100);
+    return searchSession.search(Property.class).where(f -> f.bool()
+            .must(f.match().fields("title", "description", "city").matching(effectiveKeyword))
+            .must(f.range().field("price").between(effectiveMinPrice, effectiveMaxPrice))
+            .must(f.range().field("createdAt").between(effectiveStartDate, effectiveEndDate)))
+        .fetchHits(100);
   }
 }
