103ce4ee3ebd9df9682df00848c7afc4f671fb12
ddasfasdfs <glibsoldatenko@gmail.com>
2024-12-13 18:22:00 +0300

    feat: add description for file validation exception

--- real-estate/src/main/java/org/apartment/controller/advice/RealEstateExceptionHandler.java
+++ real-estate/src/main/java/org/apartment/controller/advice/RealEstateExceptionHandler.java
@@ -1,7 +1,9 @@
 package org.apartment.controller.advice;
 
+import org.apartment.dto.ResponseDto;
 import org.apartment.exception.FileValidationException;
 import org.apartment.exception.GlobalExceptionHandler;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class RealEstateExceptionHandler extends GlobalExceptionHandler {
   @ExceptionHandler(FileValidationException.class)
-  public ResponseEntity<String> handleFileValidationException(FileValidationException ex) {
-    return ResponseEntity.badRequest().body(ex.getMessage());
+  public ResponseEntity<ResponseDto<String>> handleFileValidationException(FileValidationException ex) {
+    ResponseDto<String> response = new ResponseDto<>("File validation failed: " + ex.getMessage());
+    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
   }
 }
