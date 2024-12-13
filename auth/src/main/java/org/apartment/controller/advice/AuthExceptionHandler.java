8489fc9943cbe205847a65ef51eb4e1a72d32253
ddasfasdfs <glibsoldatenko@gmail.com>
2024-12-13 18:04:08 +0300

    feat: add GlobalExceptionHandler starter in auth

--- auth/src/main/java/org/apartment/controller/advice/AuthExceptionHandler.java
+++ auth/src/main/java/org/apartment/controller/advice/AuthExceptionHandler.java
@@ -3,66 +3,25 @@ package org.apartment.controller.advice;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
-import jakarta.validation.ConstraintViolationException;
 import java.security.SignatureException;
-import java.util.HashMap;
-import java.util.Map;
 import org.apartment.dto.ResponseDto;
+import org.apartment.exception.GlobalExceptionHandler;
 import org.apartment.exception.InvalidAuthorizationHeaderException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
-  @ExceptionHandler(MethodArgumentNotValidException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ResponseEntity<ResponseDto<Map<String, String>>> handleValidationExceptions(
-      MethodArgumentNotValidException ex) {
-    Map<String, String> errors = new HashMap<>();
-    ex.getBindingResult().getAllErrors().forEach((error) -> {
-      String fieldName = ((FieldError) error).getField();
-      String errorMessage = error.getDefaultMessage();
-      errors.put(fieldName, errorMessage);
-    });
-    ResponseDto<Map<String, String>> response = new ResponseDto<>(errors);
-    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-  }
-
-  @ExceptionHandler(ConstraintViolationException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ResponseEntity<ResponseDto<String>> handleConstraintViolationException(
-      ConstraintViolationException ex) {
-    ResponseDto<String> response = new ResponseDto<>("Constraint violation: " + ex.getMessage());
-    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-  }
-
-  @ExceptionHandler(IllegalStateException.class)
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  public ResponseEntity<ResponseDto<String>> handleIllegalStateException(IllegalStateException ex) {
-    ResponseDto<String> response = new ResponseDto<>(ex.getMessage());
-    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-  }
-
+public class AuthExceptionHandler extends GlobalExceptionHandler {
   @ExceptionHandler(InvalidAuthorizationHeaderException.class)
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public ResponseEntity<ResponseDto<String>> handleInvalidAuthorizationHeaderException(
       InvalidAuthorizationHeaderException ex) {
     ResponseDto<String> response = new ResponseDto<>(ex.getMessage());
     return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-  }
-
-  @ExceptionHandler(Exception.class)
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-  public ResponseEntity<ResponseDto<String>> handleGenericException(Exception e) {
-    ResponseDto<String> response =
-        new ResponseDto<>("An unexpected error occurred: " + e.getMessage());
-    return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
   @ExceptionHandler(ExpiredJwtException.class)
